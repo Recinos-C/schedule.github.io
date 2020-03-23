b528b95436f8180e4492a3a661b4a248493142ff
Chris Recinos <recinos.c.156@gmail.com>
2020-03-22 20:18:08 -0700

    update targeting function

--- javasrcpt.js
+++ javasrcpt.js
@@ -1,49 +1,64 @@
 
-    for (i=0;i<2;i++){
-        var rowDiv = $("<div>");
-        var colDiv = $("<div>");
-        var formDiv = $("<form>");
-        var buttonDiv = $("<button>");
-        var textArea = $("<textarea>");
-        var title = $("<h3>");
-        var timeAm = 12;
-        var timePm = 1;
+    // for (i=0;i<25;i++){
+    //     var rowDiv = $("<div>");
+    //     var colDiv = $("<div>");
+    //     var formDiv = $("<form>");
+    //     var buttonDiv = $("<button>");
+    //     var textArea = $("<textarea>");
+    //     var title = $("<h3>");
+    //     var timeAm = 12;
+    //     var timePm = 1;
 
-        $(".container").append(rowDiv);
-        $(rowDiv).addClass("card-fluid");
-        $(rowDiv).append("<div>");
-        $("div").last().addClass("row no-gutters");
-        $(".row no-gutters").append("<div>");
+    //     $(".container").append(rowDiv);
+    //     $(rowDiv).addClass("card-fluid");
+    //     $(rowDiv).append("<div>");
+    //     $("div").last().addClass("row no-gutters");
+    //     $(".row no-gutters").append("<div>");
 
 
-        $("div").last().addClass("col-md-2 bg-dark");
-        $(".col-md-2 bg-dark").append(title);
-        $(title).addClass("time-text time-text text-center");
-        $(".row no-gutters").append("<div>");
-        $("div").last().addClass("col-md-10");
-        $(".col-md-10").append(formDiv);
-        $(formDiv).addClass("form-inline");
-        $(formDiv).append(textArea);
-        $(textArea).addClass("form-control col-lg-10");
-        $(textArea).attr("id", "event");
-        $(textArea).attr("rows", "4");
-        $(formDiv).append(buttonDiv);
-        $(buttonDiv).addClass("btn col-md-2 btn-squared-default btn-dark");
-        $(buttonDiv).append("<i>");
-        $("i").last().addClass("fa fa-lock fa-5x");
-        $("i").attr("data-img=","icons");
-        $("i").last().append("<br>").text("Locked");
-        $("i").last().append("<br>");
-    }
-    
+    //     $("div").last().addClass("col-md-2 bg-dark");
+    //     $(".col-md-2 bg-dark").append(title);
+    //     $(title).addClass("time-text time-text text-center");
+    //     $(".row no-gutters").append("<div>");
+    //     $("div").last().addClass("col-md-10");
+    //     $(".col-md-10").append(formDiv);
+    //     $(formDiv).addClass("form-inline");
+    //     $(formDiv).append(textArea);
+    //     $(textArea).addClass("form-control col-lg-10");
+    //     $(textArea).attr("id", "event");
+    //     $(textArea).attr("rows", "4");
+    //     $(formDiv).append(buttonDiv);
+    //     $(buttonDiv).addClass("btn col-md-2 btn-squared-default btn-dark");
+    //     $(buttonDiv).append("<i>");
+    //     $("i").last().addClass("fa fa-lock fa-5x");
+    //     $("i").attr("data-img=","icons");
+    //     $("i").last().append("<br>").text("Locked");
+    //     $("i").last().append("<br>");
+    // }
+   
     $("h2").text("Today is " + moment().format('MM Do YYYY, h:mm a'));
 var schedule = [];
+var schduleTime = $("h3").val();
+console.log($("h3").val())
+
+function start(){
+    if ( moment().format("h a") > schduleTime ){
+    console.log("you missed this event");
+    $("h3").addClass("text-primary");
+    }
+    else if ( moment().format("h a") < schduleTime){
+        console.log("You have an open schedule!")
+    }
+
+
+}
+
 
 $(".container").on("click", ".btn", function(event){
     console.log($(this).attr("id"));
 
     if ($(this).attr("id") === "clear-btn"){
-        // $(".form-inline").reset();
+        $(".form-inline").reset();
         schedule = [];
     }
     else{
@@ -57,3 +72,4 @@ $(".container").on("click", ".btn", function(event){
     }
 });
 
+start();
